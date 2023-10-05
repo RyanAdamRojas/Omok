ee1cec539684bc11d4f80b3231325e10bcd7195f
SophiaMontenegro <sophiaazulmontenegro@gmail.com>
2023-10-04 22:49:29 -0600

    Created MainOmok and updated GUI

--- src/noapplet/example/GUI.java
+++ src/noapplet/example/GUI.java
@@ -1,4 +1,22 @@
 package noapplet.example;
 
-public class GUI extends AnimationNoApplet {
+public class GUI {
+    private int width, height;
+    private char[][] board= new char[width][height];
+    public GUI(){
+        width = 15;
+        height = 15;
+    }
+    public void drawBoard(){
+        System.out.println("GAME BOARD");
+
+        for (int i = 1; i < height; i++){
+            for( int j = 1; j < width; j++){
+
+            }
+        }
+    }
+    public void drawStone(Player player, int x, int y){
+
+    }
 }
---
+++ src/noapplet/example/MainOmok.java
@@ -0,0 +1,17 @@
+package noapplet.example;
+
+import java.util.Scanner;
+
+public class MainOmok {
+    public MainOmok(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("WELCOME TO OMOK!");
+        System.out.println("PLEASE SELECT A GAME MODE [P] FOR PLAYER VS PLAYER AND [C] FOR PLAYER VS COMPUTER:");
+        String choice = scan.nextLine();
+
+
+    }
+    public static void main(String[] args) {
+        new MainOmok();
+    }
+}
