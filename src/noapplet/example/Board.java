27cccd3541aab861b27397a94d17e8a7b7e813ea
Ryan Rojas <ryanadamrojas@protonmail.com>
2023-10-06 16:10:56 -0600

    Board.didWin finished, needs testing.

--- src/noapplet/example/Board.java
+++ src/noapplet/example/Board.java
@@ -2,7 +2,7 @@
 package noapplet.example;
 
 public class Board {
-    private String[][] cells;
+    private String[][] cells; // [col][row]
     private int size = 15;
     private int activeStoneCount = 0;
     private int maxStoneCount = 225;
@@ -19,9 +19,9 @@ public class Board {
             this.cells = new String[this.size][this.size];
 
             // Cells initialized as null
-            for (int i = 0; i < this.size; i++) {
-                for (int j = 0; j < this.size; j++) {
-                    cells[i][j] = null;
+            for (int col = 0; col < this.size; col++) {
+                for (int row = 0; row < this.size; row++) {
+                    cells[col][row] = null;
                 }
             }
         }
@@ -40,42 +40,38 @@ public class Board {
     private boolean isCellAvailable(int x, int y){
         // If coordinates are out of bounds
         if ((x*y) > maxStoneCount) return false;
-        // -1 for coordinate to array index offset
-        if (cells[x-1][y-1] == null) return true;
-        else return false;
+        // -1 for coordinate to array index offset, [col][row]
+        return cells[y - 1][x - 1] == null;
     }
 
     private void placeStone(int x, int y, String symbol) {
         // -1 for coordinate to array index offset
-        this.cells[x-1][y-1] = symbol;
+        this.cells[y-1][x-1] = symbol;
         this.activeStoneCount++;
         if (activeStoneCount == maxStoneCount) isFull = true;
     }
 
     private boolean didWin(String symbol){ // TODO CHECK IF 5 STONES IN A ROW
-
-        return streakDiagnalRight(symbol) || streakDiagnalLeft(symbol) || streakRows(symbol) || streakColumns(symbol);
+        return streakDiagonalRight(symbol) || streakDiagonalLeft(symbol) || streakRows(symbol) || streakColumns(symbol);
     }
 
-    private boolean streakDiagnalRight(String symbol){
-        // TODO TEST
+    private boolean streakDiagonalRight(String symbol){
         int count = 0;
         // Loop through the 2D array 'cells'
-        for (int i = 0; i < this.size; i++) {
-            for (int j = 0; j < this.size; j++) {
+        for (int col = 0; col < this.size; col++) {
+            for (int row = 0; row < this.size; row++) {
+                count = 0; // Resets count when starting a new right diagonal
 
-                // Reseting count when starting a new diagonal
-                count = 0;
-
-                // Check for winning streak in diagnal right and downward  from current (i, j)
+                // Check for winning streak in diagonal right and downward  from current cell
                 for (int k = 0; k < 5; k++) {  // Winning streak is k = 5
-                    if (i + k < this.size && j + k < this.size) { // Stay within bounds
-                        if (cells[i + k][j + k].equals(symbol)) {
+                    if (col + k < this.size && row + k < this.size) { // Stay within bounds
+                        String currentCell = cells[col + k][row + k];
+                        if (currentCell.equals(symbol)) {
                             count++;
-                            if (count == 5) { // Found 5 in a row
-                                return true;
-                            }
-                        } else {
+                            if (count == 5)
+                                return true; // Found 5 in a row!!!
+                        }
+                        else {
                             count = 0; // Resets count anytime a non-matching cell is found
                         }
                     }
@@ -85,18 +81,77 @@ public class Board {
         return false;
     }
 
-    private boolean streakDiagnalLeft(String symbol){
-        // TODO
+    private boolean streakDiagonalLeft(String symbol){
+        int count = 0;
+        // Loop through the 2D array 'cells'
+        for (int col = (this. size - 1); col >= 0; col--) {
+            for (int row = (this.size -1); row >= 0; row--) {
+                count = 0; // Resets count when starting a new left diagonal
+
+                // Check for winning streak in diagonal left and downward from current cell
+                for (int k = 0; k < 5; k++) {  // Winning streak is k = 5
+                    if (col - k <= 0 && row - k <= 0) { // Stay within bounds
+                        String currentCell = cells[col - k][row - k];
+                        if (currentCell.equals(symbol)) {
+                            count++;
+                            if (count == 5)
+                                return true; // Found 5 in a row!!!
+                        }
+                        else {
+                            count = 0; // Resets count anytime a non-matching cell is found
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
     private boolean streakRows(String symbol){
-        // TODO
+        int count = 0;
+        for (int col = 0; col < this.size; col++) {
+            for (int row = 0; row < this.size; row++) {
+                count = 0;  // Resets count when starting a new row
+
+                // Check for winning streak right from current cell
+                for (int k = 0; k < 5; k++) {  // Winning streak is k = 5
+                    if (row + k < size) { // Stay within bounds in row
+                        String currentCell = cells[col][row + k]; // From column, checks down the row
+                        if (currentCell.equals(symbol)) {
+                            count++;
+                            if (count == 5) return true; // Found 5 in a row!!!
+                        }
+                        else {
+                            count = 0; // Resets count anytime a non-matching cell is found
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
     private boolean streakColumns(String symbol){
-        // TODO
+        int count = 0;
+        for (int col = (size - 1); col >= 0; col--) {
+            for (int row = (size -1); row >= 0; row--) {
+                count = 0; // Resets count when starting a new diagonal
+
+                // Check for winning streak down from current cell
+                for (int k = 0; k < 5; k++) {  // Winning streak is k = 5
+                    if (col + k < size) { // Stay within bounds in column
+                        String currentCell = cells[col + k][row]; // From row, checks down the column
+                        if (currentCell.equals(symbol)) {
+                            count++;
+                            if (count == 5) return true; // Found 5 in a row!!!
+                        }
+                        else {
+                            count = 0; // Resets count anytime a non-matching cell is found
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
